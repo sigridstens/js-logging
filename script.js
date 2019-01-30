3aea8f18fc2412e1745d83e9f2240a7d3c6c65ba
Sigrid Stensvold <sigridstens@gmail.com>
2019-01-30 17:11:04 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById('BtnDonate').addEventListener('click', function(){
+    function donateButton() {
+        //A log for when the donate button is clicked
+        console.log("Hello World!");
+    }
+
+    donateButton();
+});
